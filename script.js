3640242e2d69acedd4c1d1b752d8866dc06d9da1
Kaleek03 <Kadec208@gmail.com>
2023-11-08 07:30:04 -0700

    class constructor

---
+++ script.js
@@ -0,0 +1,16 @@
+
+class Library {
+    constructor(
+        title = 'Unknown',
+        author = 'Unknown',
+        pages = 0,
+        read = false
+
+    ) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read
+
+    }
+}
